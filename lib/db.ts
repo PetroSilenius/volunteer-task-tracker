ad5e1be6b482fc19e2a033f3c9be59d75ce7b943
Petro Silenius <petro.silenius@gmail.com>
2023-05-21 01:29:17 +0300

    Create database SELECT helpers

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+import { sql } from "@vercel/postgres";
+
+export const getCompletedTasksForMonth = async (month: Date) => 
+    await sql<Task>`SELECT * FROM tasks WHERE EXTRACT(YEAR FROM completed_date) = ${month.getFullYear()} AND EXTRACT(MONTH FROM completed_date) = ${month.getMonth() + 1}`;
+
+export const getCompletedTasksForYear = async(year: Date) => await sql<Task>`SELECT * FROM tasks WHERE EXTRACT(YEAR FROM completed_date) = ${year.getFullYear()}`;
+
+export const getPlannedTasksForYear = async(year: Date) => await sql<Task>`SELECT * FROM tasks WHERE EXTRACT(YEAR FROM planned_date) = ${year.getFullYear()} OR completed_date IS NULL ORDER BY planned_date`;
+
+export const getTaskById = async(id: number) => await sql<Task>`SELECT * FROM tasks WHERE id = ${id}`;
